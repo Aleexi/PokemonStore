c982ba221772ffe05625fc994bf6e416340dd14c
Alexander Neumann <alexanderneumannab@gmail.com>
2024-08-11 21:22:45 +0200

    Added the Gateway (service)

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+// Services
+
+// Configuration for proxy server
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+
+        options.RequireHttpsMetadata = false;
+
+        options.TokenValidationParameters.ValidateAudience = false;
+
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapReverseProxy();
+
+app.Run();
